6937ac31916059fec9d8d324906de5a2e1a1dbab
Mit Gajera <gajeramit180@gmail.com>
2026-04-17 15:29:34 +0530

    chore: add content constants

---
+++ src/constants/content.ts
@@ -0,0 +1,11 @@
+export const UMBRA_WALLET_URL = 'https://umbraprivacy.com'
+export const UMBRA_INSTALL_URL = 'https://chromewebstore.google.com/detail/umbra-wallet/ijpkemehofemfamgfpiheecgdjgbeafk'
+export const UMBRA_DOCS_URL = 'https://docs.umbraprivacy.com/'
+export const HUSH_GITHUB_URL = 'https://github.com/mitgajera/hush'
+
+export const COPY = {
+  appName: 'Hush',
+  tagline: 'Every Solana payment is public. Hush changes that.',
+  poweredBy: 'Powered by Umbra · Arcium MPC network',
+  privateBanner: 'All payments are private by default',
+}
